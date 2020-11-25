2589f781bed6eb06520fa22a4193a93af8e98ee0
john <jonwanjema@gmail.com>
2020-11-25 13:35:23 +0300

    add statscard file

--- lib/screens/moodstats.dart
+++ lib/screens/moodstats.dart
@@ -1,3 +1,4 @@
+import 'package:book_keepa/widgets/statsCard.dart';
 import 'package:flutter/material.dart';
 
 class moodStats extends StatelessWidget {
@@ -125,42 +126,4 @@ class _moodsStatsState extends State<moodsStats> {
   }
 }
 
-class statsCard extends StatelessWidget {
-  final String title;
-  final Color color;
-  final String count;
-  const statsCard({
-    Key key,
-    @required this.size, this.title, this.color, this.count,
-  }) : super(key: key);
 
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size; //give height and width of device
-    return Container(
-      padding: EdgeInsets.all(20),
-      height: size.height * .05,
-      decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(13)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text( title,
-              textAlign: TextAlign.center,
-              style:
-                  TextStyle(fontSize: 16, color: Colors.white)),
-          SizedBox(
-            height: 5,
-          ),
-          Text(count,
-              textAlign: TextAlign.center,
-              style:
-                  TextStyle(fontSize: 23, color: Colors.white)),
-        ],
-      ),
-    );
-  }
-}
---
+++ lib/widgets/statsCard.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class statsCard extends StatelessWidget {
+  final String title;
+  final Color color;
+  final String count;
+  const statsCard({
+    Key key,
+    @required this.size, this.title, this.color, this.count,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size; //give height and width of device
+    return Container(
+      padding: EdgeInsets.all(20),
+      height: size.height * .05,
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(13)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text( title,
+              textAlign: TextAlign.center,
+              style:
+              TextStyle(fontSize: 16, color: Colors.white)),
+          SizedBox(
+            height: 5,
+          ),
+          Text(count,
+              textAlign: TextAlign.center,
+              style:
+              TextStyle(fontSize: 23, color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
