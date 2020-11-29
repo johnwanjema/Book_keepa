1da1b25df2bafdb7ba974a782aafd8d2c099da4f
john <jonwanjema@gmail.com>
2020-11-29 14:11:07 +0300

    pass color to bottom nav item

--- lib/widgets/bottomNavItem.dart
+++ lib/widgets/bottomNavItem.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 class BottomNavItem extends StatelessWidget {
   final IconData icon;
+  final Color color;
   const BottomNavItem({
     Key key,
-    this.icon,
+    this.icon, this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(child: Icon(icon));
+    return GestureDetector(
+        child: Icon(
+      icon,
+      size: 30,
+      color: color != null ? color : Colors.black,
+    ));
   }
 }
